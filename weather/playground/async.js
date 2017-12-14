9595f7d12eea812725120e8431459e840523eaec
siyaths19@gmail.com <siyaths19@gmail.com>
2017-12-14 23:19:32 +0530

    Weather App with Nodejs

---
+++ weather/playground/async.js
@@ -0,0 +1,11 @@
+console.log("Satrting App");
+
+setTimeout(()=>{
+    console.log('Hola is printing after 2 seconds');
+}, 2000);
+
+setTimeout(()=>{
+ console.log("printing under 0 sec");
+},0)
+
+console.log('Ending App');
